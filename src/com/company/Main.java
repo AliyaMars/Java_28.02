d66ed699f1b3bea07805cf67292f70ffccbb8a3b
AliyaMars <safarova-aliya@list.ru>
2022-02-28 13:00:01 +0300

    Задание 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -31,3 +31,16 @@ class DivisibleNb {
         }
     }
 }
+//Задание 3
+//You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+class Solution {
+    public static int areaOrPerimeter (int l, int w) {
+        if(l==w){
+            return w*w;
+        }
+        else{
+            return w*2+l*2;
+        }
+    }
+}
