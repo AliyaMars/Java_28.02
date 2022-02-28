153f9002d12447bf7ef220458994c685ce0d10f8
AliyaMars <safarova-aliya@list.ru>
2022-02-28 12:54:24 +0300

    Задание 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,4 +19,15 @@ class Kata{
     }
 }
 //Задание 2
-//
+//Create a function that checks if a number n is divisible by two numbers x AND y.
+// All inputs are positive, non-zero digits.
+class DivisibleNb {
+    public static boolean isDivisible(long n, long x, long y) {
+        if((n%x==0) && (n%y==0)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
